9278d5571a22326136504448ba14c622642ccf0b
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-08-02 11:41:11 +0100

    add recipes data and seeding

--- db/data/run-seed.js
+++ db/data/run-seed.js
@@ -9,18 +9,32 @@ const seedDatabase = async () => {
       "utf8"
     );
     const usersData = JSON.parse(jsonUsersData);
+
     const jsonEventsData = await fs.readFile(
       `${__dirname}/test-data/events.json`,
       "utf8"
     );
     const eventsData = JSON.parse(jsonEventsData);
+
+    const jsonRecipesData = await fs.readFile(
+      `${__dirname}/test-data/recipes.json`,
+      "utf8"
+    );
+    const recipesData = JSON.parse(jsonRecipesData);
+
     const client = await connectToDatabase();
+
     const usersCollection = client.db().collection("users");
     const eventsCollection = client.db().collection("events");
+    const recipesCollection = client.db().collection("recipes");
+
     await usersCollection.deleteMany();
     await eventsCollection.deleteMany();
+    await recipesCollection.deleteMany();
+
     await usersCollection.insertMany(usersData);
     await eventsCollection.insertMany(eventsData);
+    await recipesCollection.insertMany(recipesData);
   } catch (error) {
     console.error("Error seeding database:", error);
   }
