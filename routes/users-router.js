24304fec415ac14ce64129458407796488b5130c
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-08-02 09:16:41 +0100

    add a patchUser path to the router

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,6 +1,8 @@
 const usersRouter = require("express").Router();
-const { postUser, getUser } = require("../controllers/users-controller");
+const { postUser } = require("../controllers/users-controller");
+const { patchUser } = require("../controllers/users-controller");
 
-usersRouter.route("/").post(postUser);
+usersRouter.route("/").post(postUser)
+usersRouter.route("/:_id").patch(patchUser)
 
 module.exports = usersRouter;
