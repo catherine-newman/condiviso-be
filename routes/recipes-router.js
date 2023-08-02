cdb13512f58e85249cd53f83421a239e0cb6adf9
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-08-02 12:26:19 +0100

    add GET /api/recipes/:recipe_id

--- routes/recipes-router.js
+++ routes/recipes-router.js
@@ -1,3 +1,6 @@
 const recipesRouter = require("express").Router();
+const { getRecipe } = require("../controllers/recipes.controller");
+
+recipesRouter.route("/:recipe_id").get(getRecipe);
 
 module.exports = recipesRouter;
