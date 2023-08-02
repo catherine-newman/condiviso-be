46c9033b18432e40fa77cf60da9181eb6ae26563
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-08-02 09:19:36 +0100

    add passing controller/model for patchUser

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,7 +1,9 @@
-const { addUser, findUser } = require("../models/users-model");
+const { addUser} = require("../models/users-model");
+const { updateUser} = require("../models/users-model");
 
 exports.postUser = (req, res, next) => {
-  const {
+    
+      const {
     first_name,
     last_name,
     email,
@@ -30,3 +32,21 @@ exports.postUser = (req, res, next) => {
       return next(err);
     });
 };
+
+
+exports.patchUser = (req, res, next) => {
+        const { _id } = req.params;
+        const updateData = req.body;
+        
+       
+    updateUser(_id, updateData)
+      .then((updatedUser) => {
+        res.status(200).send({user: updatedUser})
+      })
+      .catch((err) => {
+      console.log(  Object.keys(err), 'here')
+        return next(err)
+        })
+      }
+  
+
--- models/users-model.js
+++ models/users-model.js
@@ -1,4 +1,5 @@
 const { connectToDatabase } = require("../db/connection");
+const { ObjectId } = require("mongodb");
 
 exports.addUser = async (
   first_name,
@@ -30,7 +31,8 @@ exports.addUser = async (
     return Promise.reject({ status: 400, msg: "Bad Request" });
   }
   try {
-    const client = await connectToDatabase();
+    const client = await connectToDatabase(); 
+    console.log(client)
     const collection = client.db().collection("users");
     findResult = await collection.findOne({ user_name: user_name });
     if (findResult) {
@@ -50,6 +52,69 @@ exports.addUser = async (
     }
   } catch (err) {
     console.error("Error accessing the database:", err);
+    throw err;
+  }
+};
+
+
+
+exports.updateUser = async (_id, updateData) => {
+
+  try{
+    
+    const client = await connectToDatabase();
+    const usersCollection = client.db().collection("users");
+    const user = await usersCollection.findOne({ _id: _id });
+        if (!user) {
+      return  Promise.reject({ status: 404, msg: "User not found" });
+        }      
+        const username = updateData.user_name    
+        
+        const usernameTaken = await usersCollection.findOne({ user_name: username });
+        if (usernameTaken) {
+          return Promise.reject({ status: 409, msg: "Username already exists" });
+        }
+      
+
+
+    const validKeys = ['first_name', 'last_name', 'email', 'user_name', 'address', 'postcode', 'about_me', 'recipes']
+    
+    const invalidKeys = Object.keys(updateData).filter(key => !validKeys.includes(key));
+    
+    if (invalidKeys.length > 0) {
+      return  Promise.reject({ status: 400, msg: "Bad Request" });
+    }
+    const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const postcodeRegex = /^([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$/;
+    
+        if (updateData.email && !emailRegex.test(updateData.email)) {
+          return Promise.reject({ status: 400, msg: "Bad Request" });
+        }       
+
+        if (updateData.postcode && !postcodeRegex.test(updateData.postcode)) {
+          return Promise.reject({ status: 400, msg: "Bad Request" });
+        }
+      
+    
+  const updateKey = {};
+  if (updateData.first_name) updateKey.first_name = updateData.first_name;
+  if (updateData.last_name) updateKey.last_name = updateData.last_name;
+  if (updateData.email) updateKey.email = updateData.email;
+  if (updateData.user_name) updateKey.user_name = updateData.user_name;
+  if (updateData.address) updateKey.address = updateData.address;
+  if (updateData.postcode) updateKey.postcode = updateData.postcode;
+  if (updateData.about_me) updateKey.about_me = updateData.about_me;
+  if (updateData.recipes) updateKey.recipes = updateData.recipes;
+  
+ 
+
+    await usersCollection.updateOne({ _id: _id }, { $set: updateKey });
+
+    const updatedUser = await usersCollection.findOne({ _id: _id });
+       return updatedUser;
+  }catch (err) {
+    console.error("Error updating user:", err);
     throw err;
   }
 };
