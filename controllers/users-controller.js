f50073c581f64e1299adf025ed0e5ce36a86a42f
stephen <stephenlancaster1981@hotmail.co.uk>
2023-08-08 23:29:31 +0100

    added getUserByUsername function

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,10 @@
 const { addUser } = require("../models/users-model");
 const { updateUser } = require("../models/users-model");
 const { findUser } = require("../models/users-model");
+const { findUsername } = require("../models/users-model")
+const { ObjectId } = require("mongodb");
+const {findUserById} = require("../models/users-model")
+const {findUserByUsername} = require("../models/users-model")
 
 exports.postUser = (req, res, next) => {
   const {
@@ -46,17 +50,52 @@ exports.patchUser = (req, res, next) => {
     });
 };
 
-exports.getUser = (req, res, next) => {
-  const { user_id } = req.params;
-  findUser(user_id)
-    .then((data) => {
-      if (!data) {
-        return Promise.reject({ status: 404, msg: "User not found" });
-      }
+// exports.getUser = (req, res, next) => {
+//   const { user_id } = req.params;
+//   findUser(user_id)
+//     .then((data) => {
+//       if (!data) {
+//         return Promise.reject({ status: 404, msg: "User not found" });
+//       }
 
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      return next(err);
-    });
+//       res.status(200).send(data);
+//     })
+//     .catch((err) => {
+//       return next(err);
+//     });
+// };
+
+// exports.getUsername = async (req, res, next) => {
+//   console.log("in controller <<<<<<")
+//   const { user_name } = req.params;
+//   try {
+//     const data = await findUsername(user_name);
+//     res.status(200).send({ user : data });
+//   } catch (err) {
+//     return next(err);
+//   }
+// }
+exports.getUser = async (req, res, next) => {
+  const { user_param } = req.params;
+  try {
+    if (ObjectId.isValid(user_param)) {
+      const data = await findUserById(user_param);
+      if (data === null){
+        res.status(404).send({ msg: "User not found"})
+      }else{
+        res.status(200).send({ user: data });
+      }
+    }                 // IF THIS CHECKS OUT REMOVE THE REQUIRES FROM THE TOP
+     else {
+      const data = await findUserByUsername(user_param);
+      if(data === null){
+        res.status(400).send({ msg: "Bad Request"});
+      }else{
+      res.status(200).send({ user: data });
+    }
+  } 
+}
+catch (err) {
+  return next(err);
+}
 };
